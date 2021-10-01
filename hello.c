0f9eb7b1780f2c1271309b627de5858633e7fa5d
anandhu-kb <56553335+anandhu-kb@users.noreply.github.com>
2021-10-01 10:07:32 +0530

    @just_me

---
+++ hello.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+	char var1, var2, var3, var4, var5, var6, var7, var8, var9, var10, var11, var12, var13, var14, var15, var16, var17, var18, var19, var20, var21, var22, var23, var24, var25, var26;
+	var1 = 'a';
+	var2 = 'b';
+	var3 = 'c';
+	var4 = 'd';
+	var5 = 'e';
+	var6 = 'f';
+	var7 = 'g';
+	var8 = 'h';
+	var9 = 'i';
+	var10 = 'j';
+	var11 = 'k';
+	var12 = 'l';
+	var13 = 'm';
+	var14 = 'n';
+	var15 = 'o';
+	var16 = 'p';
+	var17 = 'q';
+	var18 = 'r';
+	var19 = 's';
+	var20 = 't';
+	var21 = 'u';
+	var22 = 'v';
+	var23 = 'x';
+	var24 = 'y';
+	var25 = 'z';
+
+
+	printf("%c%c%c%c%c%c%c", var1, var14, var1, var14, var4, var8, var21); // 1 14 1 14 4 8 21
+	return 0;
+}
